8c1b41ec775e7f022f56e8d39bec77f71b41b5e5
dudu <hellodudu86@gmail.com>
2019-01-05 15:44:50 +0800

    bug:tcp handle cpu 100%

--- server/tcp_server.go
+++ server/tcp_server.go
@@ -63,16 +63,21 @@ func handleTcpConnection(con net.Conn) {
 	})
 
 	for {
-		if scanner.Scan() {
-			GetUltimateAPI().AddTask(func() {
-				GetUltimateAPI().GetWorldSession().HandleMessage(con, scanner.Bytes())
-			})
+		select {
+		default:
+			if scanner.Scan() {
+				byMsg := scanner.Bytes()
+				GetUltimateAPI().AddTask(func() {
+					GetUltimateAPI().GetWorldSession().HandleMessage(con, byMsg)
+				})
+			}
+
+			// end of connection
+			if err := scanner.Err(); err != nil {
+				log.Println(color.YellowString("scan error:%s", err.Error()))
+				GetUltimateAPI().GetWorldSession().DisconnectWorld(con)
+			}
 		}
 
-		// end of connection
-		if err := scanner.Err(); err != nil {
-			log.Println(color.YellowString("scan error:%s", err.Error()))
-			GetUltimateAPI().GetWorldSession().DisconnectWorld(con)
-		}
 	}
 }

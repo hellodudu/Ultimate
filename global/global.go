a03de65902c57d30e04a5be62fa50cccda0a8a3b
hellodudu86 <hellodudu86@gmail.com>
2019-03-04 18:14:45 +0800

    reorganize code

---
+++ global/global.go
@@ -0,0 +1,43 @@
+package global
+
+import (
+	"strconv"
+
+	"github.com/gorilla/websocket"
+	"github.com/hellodudu/Ultimate/res"
+)
+
+// redis
+var RedisAddr string = "127.0.0.1:6379"
+var RedisPwd string = ""
+var RedisDB int = 0
+
+// websocket
+var Upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+// inimgr
+var IniMgr = res.NewIniMgr()
+
+func turnToInt(v string, e error) int {
+	n, _ := strconv.Atoi(v)
+	return n
+}
+
+// world config
+var (
+	WorldHeartBeatSec  = turnToInt(IniMgr.GetIniValue("config/config.ini", "world config", "WorldHeartBeatSec"))
+	WorldConTimeOutSec = turnToInt(IniMgr.GetIniValue("config/config.ini", "world config", "WorldConTimeOutSec"))
+	WorldConnectMax    = uint32(turnToInt(IniMgr.GetIniValue("config/config.ini", "world config", "WorldConnectMax")))
+)
+
+// mysql
+var (
+	MysqlUser, _ = IniMgr.GetIniValue("config/config.ini", "mysql", "user")
+	MysqlPwd, _  = IniMgr.GetIniValue("config/config.ini", "mysql", "pwd")
+	MysqlAddr, _ = IniMgr.GetIniValue("config/config.ini", "mysql", "addr")
+	MysqlPort, _ = IniMgr.GetIniValue("config/config.ini", "mysql", "port")
+	MysqlDB, _   = IniMgr.GetIniValue("config/config.ini", "mysql", "db")
+)

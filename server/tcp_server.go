cf4d71f652a78acfaa053c6aecc03c717f9ab92c
dudu <hellodudu86@gmail.com>
2019-03-28 22:11:44 +0800

    fix bugs: tcp server unmarshal receive data error

--- server/tcp_server.go
+++ server/tcp_server.go
@@ -1,8 +1,6 @@
 package ultimate
 
 import (
-	"bufio"
-	"bytes"
 	"encoding/binary"
 	"io"
 	"net"
@@ -11,6 +9,8 @@ import (
 	"github.com/hellodudu/Ultimate/global"
 	"github.com/hellodudu/Ultimate/logger"
 )
+
+var tcpReadBufMax = 1024 * 100
 
 type TcpServer struct {
 }
@@ -46,48 +46,92 @@ func (server *TcpServer) Run() {
 		}
 
 		logger.Info("a new tcp connection!")
-		go handleTcpConnection(con)
+		go handleTCPConnection(con)
+
 	}
 }
 
-func handleTcpConnection(con net.Conn) {
-	defer con.Close()
-
-	con.(*net.TCPConn).SetKeepAlive(true)
-	con.(*net.TCPConn).SetKeepAlivePeriod(30 * time.Second)
-	scanner := bufio.NewScanner(con)
-
-	// first 4 bytes represent tcp package size, split it
-	scanner.Split(func(data []byte, atEOF bool) (advance int, token []byte, err error) {
-		if atEOF {
-			return advance, token, io.EOF
-		}
-
-		if len(data) > 4 {
-			length := uint32(0)
-			binary.Read(bytes.NewReader(data[:4]), binary.LittleEndian, &length)
-			if int(length)+4 <= len(data) {
-				return int(length) + 4, data[:int(length)+4], nil
-			}
-		}
-		return
-	})
+func handleTCPConnection(conn net.Conn) {
+	defer conn.Close()
+	conn.(*net.TCPConn).SetKeepAlive(true)
+	conn.(*net.TCPConn).SetKeepAlivePeriod(30 * time.Second)
 
 	for {
-		ok := scanner.Scan()
-		if ok {
-			byMsg := scanner.Bytes()
-			Instance().AddTask(func() {
-				Instance().GetWorldSession().HandleMessage(con, byMsg)
-			})
-		} else if err := scanner.Err(); err != nil {
-			logger.Warning("scan error:", err)
-			// end of connection
-			Instance().GetWorldSession().DisconnectWorld(con)
-			break
-		} else {
-			logger.Info("one client connection was shut down!")
-			break
+		// read len
+		b := make([]byte, 4)
+		if _, err := io.ReadFull(conn, b); err != nil {
+			logger.Info("one client connection was shut down:", err)
+			return
 		}
+
+		var msgLen uint32
+		msgLen = binary.LittleEndian.Uint32(b)
+
+		// check len
+		if msgLen > uint32(tcpReadBufMax) {
+			logger.Warning("tcp recv error:message too long")
+			continue
+		} else if msgLen < 4 {
+			logger.Warning("tcp recv error:message too short")
+			continue
+		}
+
+		// data
+		msgData := make([]byte, msgLen+4)
+		copy(msgData, b[:])
+		if _, err := io.ReadFull(conn, msgData[4:]); err != nil {
+			logger.Warning("tcp recv error:", err)
+			continue
+		}
+
+		// handle message
+		Instance().AddTask(func() {
+			Instance().GetWorldSession().HandleMessage(conn, msgData)
+		})
 	}
 }
+
+// func handleTcpConnection(con net.Conn) {
+// 	defer con.Close()
+
+// 	con.(*net.TCPConn).SetKeepAlive(true)
+// 	con.(*net.TCPConn).SetKeepAlivePeriod(30 * time.Second)
+// 	scanner := bufio.NewScanner(con)
+
+// 	// first 4 bytes represent tcp package size, split it
+// 	scanner.Split(func(data []byte, atEOF bool) (advance int, token []byte, err error) {
+// 		if atEOF {
+// 			return advance, token, io.EOF
+// 		}
+
+// 		logger.Trace("tcp recv data length = ", len(data))
+
+// 		if len(data) > 4 {
+// 			length := uint32(0)
+// 			binary.Read(bytes.NewReader(data[:4]), binary.LittleEndian, &length)
+// 			logger.Trace("tcp actual data size:", length)
+// 			if int(length)+4 <= len(data) {
+// 				return int(length) + 4, data[:int(length)+4], nil
+// 			}
+// 		}
+// 		return
+// 	})
+
+// 	for {
+// 		ok := scanner.Scan()
+// 		if ok {
+// 			byMsg := scanner.Bytes()
+// 			Instance().AddTask(func() {
+// 				Instance().GetWorldSession().HandleMessage(con, byMsg)
+// 			})
+// 		} else if err := scanner.Err(); err != nil {
+// 			logger.Warning("scan error:", err)
+// 			// end of connection
+// 			Instance().GetWorldSession().DisconnectWorld(con)
+// 			break
+// 		} else {
+// 			logger.Info("one client connection was shut down!")
+// 			break
+// 		}
+// 	}
+// }

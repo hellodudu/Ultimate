8b11e5351c0daa3c409d528069f43c222c077837
dudu <hellodudu86@gmail.com>
2019-04-05 18:03:49 +0800

    add rpc client

---
+++ client/rpc_client.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"time"
+
+	world_message "github.com/hellodudu/Ultimate/proto"
+	"google.golang.org/grpc"
+)
+
+const (
+	address     = "localhost:7080"
+	defaultName = "nice to meet you"
+)
+
+func main() {
+	// Set up a connection to the server.
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := world_message.NewGreeterClient(conn)
+
+	// Contact the server and print out its response.
+	name := defaultName
+	if len(os.Args) > 1 {
+		name = os.Args[1]
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	r, err := c.SayHello(ctx, &world_message.HelloRequest{Name: name})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	log.Printf("Greeting: %s", r.Message)
+}
